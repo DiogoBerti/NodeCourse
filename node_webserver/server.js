822be866ee035aa352b72afc8b2d20ceb05e64dd
DiogoBerti <diogoberti88@gmail.com>
2018-07-15 20:07:13 -0300

    [ADD] - Utilizando Partials para complementar os templates (views) [ADD] - Injetando dados nas pages via objetos js [ADD] - Novos arquivos hbs (handlebars)

--- node_webserver/server.js
+++ node_webserver/server.js
@@ -1,26 +1,41 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 // Utilizando o Express para criar um server web...
 
+hbs.registerPartials(__dirname + '/views/partials');
+// Chamando o handlebars para dentro do express...
+app.set('view engine', 'hbs');
 // Utilizado para apresentar arquivos estaticos pelo server... Utilizando os arquivos da pasta public
 app.use(express.static(__dirname + '/public'));
 
 // Adicionando rotas via url com respostas em Json e Texto..
 app.get('/', (req, res) => {
 	// res.send('<h1>Hello Express</h1>');
-	res.send({
-		name: 'Diogo',
-		likes: [
-			'Dogs',
-			'Cats',
-			'Guitars'
-		]
+	// res.send({
+	// 	name: 'Diogo',
+	// 	likes: [
+	// 		'Dogs',
+	// 		'Cats',
+	// 		'Guitars'
+	// 	]
+	// });
+	res.render('home.hbs', {
+		pageTitle: 'Home Page Test',
+		currentYear: new Date().getFullYear(),
+		welcomeMessage: 'Welcome to My Website you shithead!'
 	});
+
 });
 
 app.get('/about', (req, res) =>{
-	res.send('<h1>About Page</h1>');
+	// O Primeiro argumento do Render é a page, o segundo é o objeto que será enviado como valores para renderização
+	res.render('about.hbs', {
+		pageTitle: 'About Page Test',
+		currentYear: new Date().getFullYear()
+	});
+	// res.send('<h1>About Page</h1>');
 });
 
 app.get('/bad', (req, res) =>{
